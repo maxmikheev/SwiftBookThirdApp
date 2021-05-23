0921b87dbadde736b79285f8bace7ea42093bc05
Maksim Mikheev <MMikheev@yotateam.com>
2021-05-23 19:29:09 +0300

    Добавил возможность менять слайдерами цвет view

--- SwiftBookThirdApp/ViewController.swift
+++ SwiftBookThirdApp/ViewController.swift
@@ -9,6 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var redSlider: UISlider! {
+        didSet{
+            redSlider.tintColor = UIColor.red
+        }
+    }
+    @IBOutlet weak var greenSlider: UISlider! {
+        didSet{
+            greenSlider.tintColor = UIColor.green
+        }
+    }
+    @IBOutlet weak var blueSlider: UISlider! {
+        didSet{
+            blueSlider.tintColor = UIColor.blue
+        }
+    }
+    
+    func changeViewColor() {
+        colorView.backgroundColor = UIColor(displayP3Red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+    
+    @IBAction func rgbSliders(_ sender: Any) {
+        changeViewColor()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
